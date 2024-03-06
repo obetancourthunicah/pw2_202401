1c26bc82dbd05e6786b010771e43cc4861919732
ULedger User <uledgeruser@ULedgers-MacBook-Pro.local>
2024-03-05 19:42:22 -0600

    Minify and Purge Vite Bundled Build

--- pw2bundled/vite.config.js
+++ pw2bundled/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import * as glob from 'glob';
+import path, { resolve } from 'node:path';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import htmlPurge from 'vite-plugin-purgecss';
 export default defineConfig(
     {
         base: '/',
-        appType: 'mpa'
+        appType: 'mpa',
+        build: {
+            rollupOptions: {
+                input: Object.fromEntries(
+                    [
+                        ...glob.sync('./!(dist)/*.html').map(
+                            file => [
+                                file.slice(0, file.length - path.extname(file).length),
+                                resolve(__dirname, file)
+                            ]
+                        ),
+                        ...glob.sync('./*.html').map(
+                            file => [
+                                file.slice(0, file.length - path.extname(file).length),
+                                resolve(__dirname, file)
+                            ]
+                        )
+                    ]
+                )
+            }
+        },
+        plugins: [
+            htmlPurge({}),
+            ViteMinifyPlugin({})
+        ]
     }
 )
